318c0065b16f0508f48daaee14924b69c0f29ec5
Philippe Loctaux <p@philippeloctaux.com>
2025-06-24 23:39:18 +0200

    remove BufferedDisplay from prelude

--- mousefood/src/prelude.rs
+++ mousefood/src/prelude.rs
@@ -7,7 +7,6 @@
 //! ```
 
 pub use crate::backend::{EmbeddedBackend, EmbeddedBackendConfig};
-pub use crate::display::BufferedDisplay;
 pub use embedded_graphics::pixelcolor::{
     Bgr555, Bgr565, Bgr666, Bgr888, Rgb555, Rgb565, Rgb666, Rgb888,
 };
